1d01a87d93eb1304fd762644f9620757aae99089
Armynerh <aimenah@outlook.com>
2023-10-20 15:43:10 -0400

    create failing test for TDD

---
+++ solver.rb
@@ -0,0 +1,14 @@
+class Solver
+    def self.factorial(n)
+      # To be implemented
+    end
+  
+    def self.reverse(word)
+      # To be implemented
+    end
+  
+    def self.fizzbuzz(n)
+      # To be implemented
+    end
+  end
+  
