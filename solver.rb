6d4d7127f28f6e9909a2a61526a2cb51c1306a29
Ulises Cappa <ulisescappa@gmail.com>
2023-10-20 18:58:25 -0300

    Complete the solver class

--- solver.rb
+++ solver.rb
@@ -1,14 +1,23 @@
 class Solver
-    def self.factorial(n)
-      # To be implemented
-    end
-  
-    def self.reverse(word)
-      # To be implemented
-    end
-  
-    def self.fizzbuzz(n)
-      # To be implemented
+  def self.factorial(n)
+    raise ArgumentError, 'No factorial defined for negative numbers' if n.negative?
+    return 1 if n.zero?
+    result = 1
+    (1..n).each { |i| result *= i }
+    result
+  end
+  def self.reverse(word)
+    word.reverse
+  end
+  def self.fizzbuzz(n)
+    if (n % 3).zero? && (n % 5).zero?
+      'fizzbuzz'
+    elsif (n % 3).zero?
+      'fizz'
+    elsif (n % 5).zero?
+      'buzz'
+    else
+      n.to_s
     end
   end
-  
+end
